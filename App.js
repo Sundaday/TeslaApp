076cc8a207752c837733960a38c5ace30ef5641f
Jonathan Koch <john.koch.pro@gmail.com>
2021-06-10 17:23:09 +0200

    button + props

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './Components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <CarItem name={"Model X"}
+        tagline={"Prix de départ à 90000€"}
+        taglineCTA={"Commandez directement"}
+        image={require('./assets/images/ModelX.jpeg')}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +20,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
---
+++ Components/CarItem/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+import StyledButton from '../StyledButton';
+import styles from './styles';
+
+const CarItem = (props) => {
+
+    const { name, tagline, taglineCTA, image } = props;
+
+    return (
+        <View style={styles.carContainer}>
+
+            <ImageBackground source={image}
+                style={styles.image}
+            />
+
+            <View style={styles.titles}>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subtitle}>{tagline}
+                    {' '}
+                    <Text style={styles.subtitleCTA}>
+                        {taglineCTA}
+                    </Text>
+                </Text>
+            </View>
+
+            <View style={styles.buttonsContainer}>
+                <StyledButton type="primary" content={"Commander"}
+                    onPress={() => {
+                        console.warn("Vous allez commandez")
+                    }} ></StyledButton>
+
+                <StyledButton type="secondary" content={"Catalogue"}
+                    onPress={() => {
+                        console.warn("Vous allez cataloguez")
+                    }} ></StyledButton>
+            </View >
+        </View >
+    )
+}
+
+export default CarItem;
