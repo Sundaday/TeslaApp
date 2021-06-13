f09a0cbac6da628de4f83bb4ddf933e36ee99f54
Jonathan Koch <john.koch.pro@gmail.com>
2021-06-13 23:48:06 +0200

    maj style.js

---
+++ Components/CarsList/cars.js
@@ -0,0 +1,21 @@
+export default [{
+        name:"Model 3",
+        tagline: "Prix de départ à 90000€",
+        taglineCTA: "Commandez directement",
+        image:require('../../assets/images/Model3.jpeg')
+    },{
+        name:"Model S",
+        tagline:"Prix de départ à 90000€",
+        taglineCTA:"Commandez directement",
+        image:require('../../assets/images/ModelS.jpeg')
+    },{
+        name:"Model Y",
+        tagline:"Prix de départ à 90000€",
+        taglineCTA:"Commandez directement",
+        image:require('../../assets/images/ModelY.jpeg')
+    },{
+        name:"Model X",
+        tagline:"Prix de départ à 90000€",
+        taglineCTA:"Commandez directement",
+        image:require('../../assets/images/ModelX.jpeg'),
+    }];
